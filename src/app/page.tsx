8c3cb08ec3f262c255fef8ad30d0e94e726e037c
attaryu <mattarannaufal@gmail.com>
2025-07-12 12:41:37 +0700

    feat: update Home component to display welcome message and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { Text } from '@/components/Text';
+
 export default function Home() {
-	return <h1>Hello world!</h1>;
+	return (
+		<main className="h-dvh p-4 flex justify-center items-center gap-4 flex-col">
+			<Text tag="h1" className="text-center">
+				Welcome to Noto!
+			</Text>
+
+			<Text className="text-center">
+				Noto is a modern, minimalistic note-taking web application. Please log
+				in to continue.
+			</Text>
+		</main>
+	);
 }
